6e3289452975cbba7d77605b2116c602edac7224
vaibhav1162 <61277103+vaibhav1162@users.noreply.github.com>
2020-04-19 15:35:35 +0530

    Update Employee.java

--- Lab_1.1 - Copy/New folder/src/main/java/com/mycompany/app/Employee.java
+++ Lab_1.1 - Copy/New folder/src/main/java/com/mycompany/app/Employee.java
@@ -40,42 +40,6 @@ public void displayAllValues() {
 	System.out.println("Employee Age : "+age);
 	
 }
-
-
-/*
-public int getEmployeeId() {
-	return employeeId;
-}
-public void setEmployeeId(int employeeId) {
-	this.employeeId = employeeId;
-}
-public String getEmployeeName() {
-	return employeeName;
-}
-public void setEmployeeName(String employeeName) {
-	this.employeeName = employeeName;
-}
-public double getSalary() {
-	return salary;
-}
-
-
-public void setSalary(double salary) {
-	this.salary = salary;
-}
-public String getBusinessUnit() {
-	return businessUnit;
-}
-public void setBusinessUnit(String businessUnit) {
-	this.businessUnit = businessUnit;
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) {
-	this.age = age;
-}
-*/
 }
 
 
